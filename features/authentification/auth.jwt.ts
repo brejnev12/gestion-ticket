3894a6d3771e3d5ee22e8679c39561761f3ef78b
Brejnev Ngondi <brejnevngondi@gmail.com>
2026-07-19 09:54:07 +0200

    feat: update secure authenfication

--- features/authentification/auth.jwt.ts
+++ features/authentification/auth.jwt.ts
@@ -1,98 +1,43 @@
 import jwt from "jsonwebtoken";
-
-
-const ACCESS_SECRET =
-  process.env.JWT_ACCESS_SECRET as string;
-
-const REFRESH_SECRET =
-  process.env.JWT_REFRESH_SECRET as string;
-
-
+const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET as string;
+const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
 if (!ACCESS_SECRET || !REFRESH_SECRET) {
-  throw new Error(
-    "Les secrets JWT sont manquants dans le fichier .env"
-  );
+  throw new Error("Les secrets JWT sont manquants dans le fichier .env");
 }
-
 
 export type AccessTokenPayload = {
   idUser: number;
   role: string;
 };
-
-
 export type RefreshTokenPayload = {
   idUser: number;
 };
 
-
-
-export function createAccessToken(
-  payload: AccessTokenPayload
-) {
-  return jwt.sign(
-    payload,
-    ACCESS_SECRET,
-    {
-      expiresIn: "15m",
-    }
-  );
+export function createAccessToken(payload: AccessTokenPayload) {
+  return jwt.sign(payload, ACCESS_SECRET, {
+    expiresIn: "15m",
+  });
 }
-
-
-
-export function createRefreshToken(
-  payload: RefreshTokenPayload
-) {
-  return jwt.sign(
-    payload,
-    REFRESH_SECRET,
-    {
-      expiresIn: "7d",
-    }
-  );
+export function createRefreshToken(payload: RefreshTokenPayload) {
+  return jwt.sign(payload, REFRESH_SECRET, {
+    expiresIn: "7d",
+  });
 }
-
-
-
-export function verifyAccessToken(
-  token: string
-): AccessTokenPayload {
-
-  const payload = jwt.verify(
-    token,
-    ACCESS_SECRET
-  );
-
-
+export function verifyAccessToken(token: string): AccessTokenPayload {
+  const payload = jwt.verify(token, ACCESS_SECRET);
   if (typeof payload === "string") {
     throw new Error("Token invalide");
   }
-
-
   return {
     idUser: payload.idUser as number,
     role: payload.role as string,
   };
 }
-
-
-
-export function verifyRefreshToken(
-  token: string
-): RefreshTokenPayload {
-
-  const payload = jwt.verify(
-    token,
-    REFRESH_SECRET
-  );
-
-
+export function verifyRefreshToken(token: string): RefreshTokenPayload {
+  const payload = jwt.verify(token, REFRESH_SECRET);
   if (typeof payload === "string") {
     throw new Error("Refresh token invalide");
   }
-
-
   return {
     idUser: payload.idUser as number,
   };
